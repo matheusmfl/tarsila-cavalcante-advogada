fec43101ba98551c909d7b6af2072c6a3daa985e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-24 15:46:01 -0300

    Finaliazed the Header content and the Hero Section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,10 +1,12 @@
+'use client'
 import Image from 'next/image'
 import heroImage from '../../../public/heroImage.png'
+import { Icon } from '@iconify/react'
 
 export function HeroSection() {
   return (
     <main className="flex flex-col px-5 pb-14 items-center justify-center bg-slate-900 overflow-y-hidden relative z-10">
-      <div className="w-80 h-80 absolute z-[-1] top-[-10px]">
+      <div className="w-80 h-80 absolute z-[-1] top-12">
         <Image
           src={heroImage}
           alt="Foto do simbolo da justiça"
@@ -20,14 +22,23 @@ export function HeroSection() {
           Enfrentando problemas para ter acesso ao Fies?
         </h1>
         <span className="text-base text-slate-50 text-center">
-          Exigir nota mínima para ingressão ao FIES é ilegal e podemos te ajudar
-          a garantir seu direito de ingressar em uma faculdade imediatamente.
+          Exigir nota mínima para ingressão ao{' '}
+          <span className="bg-orange-300/30">FIES</span> é ilegal e podemos te
+          ajudar a garantir seu{' '}
+          <span className="font-bold">
+            direito de ingressar em uma faculdade
+          </span>{' '}
+          imediatamente.
         </span>
         <button
           type="submit"
-          className=" uppercase bg-slate-950 py-3 px-4 w-full rounded-md text-orange-300"
+          className=" uppercase bg-slate-950 py-3 px-4 w-full rounded-md gap-4 text-orange-300 flex items-center justify-center"
         >
           quero ajuda especializada
+          <Icon
+            icon="ic:baseline-whatsapp"
+            className="h-6 w-6 text-orange-300"
+          />
         </button>
       </div>
     </main>
