232a642d890732b5985ee7bdbe3da637b8dae80c
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-13 16:16:16 -0300

    Create MediumScreen for CTAButton And HeroSection

--- src/app/components/divorcio/Hero.tsx
+++ src/app/components/divorcio/Hero.tsx
@@ -2,19 +2,21 @@ import { CtaButton } from './microComponents/CtaButton'
 
 export function Hero() {
   return (
-    <main className="py-24 px-6 bg-hero-bg bg-center">
+    <main className="py-24 px-6 w-full md:py-16 bg-hero-bg bg-center">
       {/* Container principal */}
       <div className="flex flex-col gap-5">
-        <h1 className="font-inter text-[28px] font-semibold leading-9 text-center text-[#FDBA74]">
+        <h1 className="font-inter text-[28px] md:text-4xl font-semibold leading-9 text-center text-[#FDBA74]">
           Simplifique o seu divórcio com a ajuda de uma Advogada especialista
         </h1>
 
-        <span className="text-slate-50 font-inter font-semibold leading-5 text-center">
+        <span className="text-slate-50 text-base md:text-xl font-inter font-semibold leading-5 text-center">
           Em um momento tão delicado, busque alguém com experiência para te
           guiar sem causar mais complicações.
         </span>
 
-        <CtaButton title="QUERO AJUDA ESPECIALIZADA" />
+        <div className="md:flex md:items-center md:justify-center">
+          <CtaButton title="QUERO AJUDA ESPECIALIZADA" />
+        </div>
       </div>
     </main>
   )
--- src/app/components/divorcio/microComponents/CtaButton.tsx
+++ src/app/components/divorcio/microComponents/CtaButton.tsx
@@ -3,7 +3,7 @@ import wpp from '../../../../assets/wppIcon.svg'
 
 export function CtaButton({ title }: { title: string }) {
   return (
-    <button className="w-full bg-[#009F16] text-slate-50 font-roboto text-base font-medium leading-5 flex gap-[10px] py-[14px] items-center justify-center rounded-[32px]">
+    <button className="w-full bg-[#009F16] md:w-[300px] text-slate-50 font-roboto text-base font-medium leading-5 flex gap-[10px] py-[14px] items-center justify-center rounded-[32px]">
       {title}
       <Image src={wpp} alt="logo do whatsapp" />
     </button>
