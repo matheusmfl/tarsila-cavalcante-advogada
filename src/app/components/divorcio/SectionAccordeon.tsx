3dbb45c5806e6bfca3e05edac4cf4746fdc68795
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-12 22:20:38 -0300

    Add CTA Button for Section Accordeon

--- src/app/components/divorcio/SectionAccordeon.tsx
+++ src/app/components/divorcio/SectionAccordeon.tsx
@@ -1,9 +1,11 @@
 import { Accordeon } from './microComponents/Accordion'
+import { CtaButton } from './microComponents/CtaButton'
 
 export function SectionAccordeon() {
   return (
-    <section className="bg-slate-100 px-6 py-10">
+    <section className="bg-slate-100 px-6 py-10 flex flex-col gap-7">
       <Accordeon />
+      <CtaButton title="QUERO AJUDA ESPECIALIZADA" />
     </section>
   )
 }
