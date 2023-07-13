6f5598659b3696632a8260197f2b848ac9343afc
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-13 17:13:07 -0300

    Create XL Screen CTAButton And HeroSection

--- src/app/components/divorcio/Hero.tsx
+++ src/app/components/divorcio/Hero.tsx
@@ -2,16 +2,17 @@ import { CtaButton } from './microComponents/CtaButton'
 
 export function Hero() {
   return (
-    <main className="py-24 px-6 w-full md:py-16 bg-hero-bg bg-center">
+    <main className="py-24 px-6 w-full md:py-16 xl:bg-no-repeat xl:px-32 xl:flex xl:items-center xl:justify-center xl:bg-cover bg-hero-bg bg-center">
       {/* Container principal */}
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 xl:gap-10 xl:py-14 ">
         <h1 className="font-inter text-[28px] md:text-4xl font-semibold leading-9 text-center text-[#FDBA74]">
-          Simplifique o seu divórcio com a ajuda de uma Advogada especialista
+          Simplifique o seu divórcio com a ajuda de uma
+          <br className="hidden xl:block" /> Advogada especialista
         </h1>
 
         <span className="text-slate-50 text-base md:text-xl font-inter font-semibold leading-5 text-center">
           Em um momento tão delicado, busque alguém com experiência para te
-          guiar sem causar mais complicações.
+          guiar sem causar <br className="hidden xl:block" /> mais complicações.
         </span>
 
         <div className="md:flex md:items-center md:justify-center">
