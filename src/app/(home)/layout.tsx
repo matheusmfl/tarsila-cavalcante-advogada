1e2831d8f67829ee6509c8fedd9608900b03373d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-09 12:01:14 -0300

    Update TagManager

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={roboto.className}>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=GTM-T5NRLNQT"
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-PJHLWH7R"
       ></Script>
       <Script
         id="TagManager"
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-T5NRLNQT');
+          })(window,document,'script','dataLayer','GTM-PJHLWH7R');
           `,
         }}
       />
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T5NRLNQT"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PJHLWH7R"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
