b09266a0a143370c6c14824642b755f37f3f2188
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-09 16:51:58 -0300

    add tagManager on trabalhista

--- src/app/(trabalhista)/layout.tsx
+++ src/app/(trabalhista)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import '../globals.css'
 import { Roboto, Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -22,8 +23,32 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${roboto.className} ${inter.className}`}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-MQSSX88V"
+      ></Script>
+      <Script
+        id="TagManager"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MQSSX88V');
+          `,
+        }}
+      />
       <link rel="icon" href="./favicon.ico" sizes="any" />
       <body className="w-full bg-white flex flex-col items-center justify-center">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MQSSX88V"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
