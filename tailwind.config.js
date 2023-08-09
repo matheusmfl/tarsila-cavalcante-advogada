22a88b8aec17433189f4c165ccb02f5ea38dc60e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-09 16:38:36 -0300

    update for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         'hero-bg': 'url(../assets/hero-bg.webp)',
         'bg-2': 'url(../assets/bg-2.webp)',
         'divider-img': 'url(../assets/divider-img.webp)',
-        'hero-trabalhista': 'url(../assets/hero-trabalhista.webp)',
+        'hero-trabalhista': 'url(../assets/hero-Trabalhista.webp)',
       },
       fontFamily: {
         inter: 'var(--font-inter)',
